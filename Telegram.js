77f400a84584423d792b890c58507b4b9b0c8944
Adarsh <adarsh.kumar02291@gmail.com>
2024-07-29 15:02:40 +0530

    added endpoint for data

--- Telegram.js
+++ Telegram.js
@@ -1,4 +1,13 @@
 const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
 
 const token = "7038548500:AAHgWxi5d1ZJHdtkNwCUFhevgwlqT4V0RGw";
 
@@ -58,31 +67,14 @@ bot.onText(/\/run/, (msg) => {
 
 bot.onText(/\/test/, (msg) => {
   const chatId = msg.chat.id;
-  console.log(msg.chat.username);
+  console.log(msg.chat);
   bot.sendMessage(chatId, "Welcome! This is test");
 });
 
 
-// function sendMessage(messageObj, messageText) {
-//   return axiosInstance.get("sendMessage", {
-//     chat_id: messageObj.chat.id,
-//     text: messageText,
-//   });
-// }
+app.get("/user/info",(req,res)=>{
+  res.send("hello")
+})
 
-// function handleMessage(messageObj) {
-//   const messageText = messageObj.text || "";
-//   if (messageText.charAt(0) === "/") {
-//     const command = messageText.substr(1);
-//     switch (command) {
-//       case "start":
-//         return;
-//       default:
-//         return sendMessage(messageObj, "Hi!, I don't know");
-//     }
-//   } else {
-//     return sendMessage(messageObj, messageText);
-//   }
-// }
 
-// module.exports = { handleMessage };
+app.listen(4040);
